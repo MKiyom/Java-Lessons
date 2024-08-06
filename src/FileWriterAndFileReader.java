2f8cd889da042725188c26ce733b6a0d81046fc6
MKiyom <kiyom.musoev@gmail.com>
2024-08-06 14:14:27 +0500

    project 4 changes made!

--- src/FileWriterAndFileReader.java
+++ src/FileWriterAndFileReader.java
@@ -1,11 +1,22 @@
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileWriterAndFileReader {
     public static void main(String[] args) throws IOException {
 
-        FileWriter fileWriter = new FileWriter("Test1.txt");
-        fileWriter.write("What is up");
+        FileWriter fileWriter = new FileWriter("fileWriteTest.txt");
+        fileWriter.write("What is up \n");
+        fileWriter.write("This is too much");
+
         fileWriter.close();
+
+        FileReader fileReader = new FileReader("fileWriteTest.txt");
+        Scanner scan = new Scanner(fileReader);
+        while (scan.hasNext()) {
+            System.out.println(scan.nextLine());
+        }
+
     }
 }
